089576d6e24c7aa660cadfe7c95370d438ee8eb6
Daniel Rios <danielrios.flamengo@gmail.com>
2021-05-21 20:47:13 -0300

    Update Adults

--- World 02/Class 13/ex054.py
+++ World 02/Class 13/ex054.py
@@ -8,11 +8,11 @@ people = [0, 0]
 today = date.today().year
 
 for count in range(0, 7):
-    person = int(input('When the person born? '))
+    person = int(input(f'When the {count + 1}º person born? '))
 
     if today - person > 18:
         people[0] += 1
     else:
         people[1] += 1
 
-print(f'{people[0]} are adults and {people[1]} are not')
+print(f'\033[34m{people[0]}\033[m are adults and \033[31m{people[1]}\033[m are not')
