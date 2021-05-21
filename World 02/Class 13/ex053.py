8b6ae614f7f8e7a5a24a8d4f38b9c9f51ab31dbe
Daniel Rios <danielrios.flamengo@gmail.com>
2021-05-21 20:40:13 -0300

    Remove Unnecessary Loop

--- World 02/Class 13/ex053.py
+++ World 02/Class 13/ex053.py
@@ -6,14 +6,7 @@ sentence = ''.join(input('Type a sentence: ').strip().lower().split())
 length = len(sentence)
 inverse = sentence[::-1]
 
-for count in range(0, (length // 2) + 1, 1):
-    palindrome = True  # The sentence is a palindrome by default
-
-    if sentence[count] != inverse[count]:  # But it verifies if really is, otherwise it is not and the loop is stopped
-        palindrome = False
-        break
-
-if palindrome is True:
+if sentence == inverse:
     print(f'This sentence \033[32mis\033[m a palindrome!')
 else:
     print(f'This sentence \033[31mis not\033[m a palindrome!')
