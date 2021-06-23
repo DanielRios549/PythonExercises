f682a0e81f7e0d69515a934d869fdeacecdcae73
Daniel Rios <danielrios.flamengo@gmail.com>
2021-06-23 12:36:52 -0300

    Group data analysis

---
+++ World 02/Class 15/ex069.py
@@ -0,0 +1,33 @@
+'''
+    Get the age a gender of an undefined number of people.
+    For each single person, the program should ask if the user wants to continue.
+    In the end, show the following:
+
+    1 - How many people are higher than 18 years old.
+    2 - How many of them are man.
+    3 - How many women are lower than 20 years old.
+'''
+division = '-=' * 20
+print(f'{division}\n{"People Register":^40}\n{division}')
+
+all18 = man = women20 = 0
+
+while True:
+    age = int(input('Age: '))
+    gender = input('Gender: [M/F]').strip().upper()
+
+    if age > 18:
+        all18 += 1
+
+    if gender == 'M':
+        man += 1
+    elif gender == 'F' and age < 20:
+        women20 += 1
+
+    again = input('Do you want to continue? [S/N]').strip().upper()
+
+    if again == 'N':
+        break
+
+print(f'People higher than \033[34m18\033[m years old: \033[32m{all18}\033[m')
+print(f'We have \033[32m{man}\033[m men at all, and \033[32m{women20}\033[m women lower than \033[34m20\033[m years old.')
