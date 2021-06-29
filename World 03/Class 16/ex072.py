8d6303ed4f10559a42c5e5bcc3d438f9344f32ad
Daniel Rios <danielrios.flamengo@gmail.com>
2021-06-29 15:23:32 -0300

    Number in Words

---
+++ World 03/Class 16/ex072.py
@@ -0,0 +1,15 @@
+'''
+    Create a tuple writing numbers in words from 0 to 20. Ask the user a number from 0 to 20,
+    and show the number in words using the item on the tuple.
+'''
+
+numbers = (
+    'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten',
+    'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen', 'twenty'
+)
+number = 0
+
+while number < 1 or number > 20:
+    number = int(input('Choose a number from 1 to 20: '))
+
+print(f'The number \033[34m{number}\033[m in words is \033[32m{numbers[number - 1]}\033[m')
