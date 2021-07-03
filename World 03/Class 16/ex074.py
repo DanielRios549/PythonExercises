00d39cdcdbb1a50451f084a6a672005dea76e272
Daniel Rios <danielrios.flamengo@gmail.com>
2021-07-03 00:16:42 -0300

    Add Important Comments

--- World 03/Class 16/ex074.py
+++ World 03/Class 16/ex074.py
@@ -6,6 +6,9 @@ from random import randint
 
 numbers = []  # This is a problem better resolved using lists instead of tuples
 
+# Another way to resolve this is using the following:
+# numbers = tuple(randint(1, 1000) for count in range(0, 5))
+
 for count in range(0, 5):
     numbers.append(randint(1, 1000))
 
