d7427887fe22bccf28d85ea0b1062ecff7ce9c64
Daniel Rios <danielrios.flamengo@gmail.com>
2021-07-02 23:56:58 -0300

    Tuple Data Analysis

---
+++ World 03/Class 16/ex075.py
@@ -0,0 +1,26 @@
+'''
+    Get four values from the user, and put them in a tuple.
+    After that show the following:
+
+    1 - How many times the number 9 appears.
+    2 - In which position appears the number 3 for the first time.
+    3 - The even numbers.
+'''
+
+values = []  # Like the previous exercise, this is better using a list.
+even = []
+
+for count in range(0, 4):
+    number = int(input('Choose a number: '))
+    values.append(number)
+
+    if number % 2 == 0:
+        even.append(number)
+
+values = tuple(values)
+even = tuple(even)
+
+print(f'The number you chose are {values}')
+print(f'The number 9 appears {values.count(9)} times')
+print(f'The number 3 appears in the index {values.index(3)} for the first time')
+print(f'The even numbers are {even}')
