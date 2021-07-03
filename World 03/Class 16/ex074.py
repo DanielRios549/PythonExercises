52874d55fa5e7b6cd4df42c09a10462bc30e635e
Daniel Rios <danielrios.flamengo@gmail.com>
2021-07-02 23:41:46 -0300

    Convert List to Tuple

--- World 03/Class 16/ex074.py
+++ World 03/Class 16/ex074.py
@@ -9,5 +9,7 @@ numbers = []  # This is a problem better resolved using lists instead of tuples
 for count in range(0, 5):
     numbers.append(randint(1, 1000))
 
+numbers = tuple(numbers)  # After creating the list, convert it to a tuple to make it immutable.
+
 print(f'The generated numbers are {numbers}')
 print(f'The lowest is {min(numbers)} and the highest is {max(numbers)}')
