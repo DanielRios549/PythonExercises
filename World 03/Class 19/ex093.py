cbc3c6e7dff3a670d0c570434c340fa5bc121414
Daniel Rios <danielrios.flamengo@gmail.com>
2021-09-05 14:14:52 -0300

    Footballer Register

---
+++ World 03/Class 19/ex093.py
@@ -0,0 +1,40 @@
+'''
+    Manage the performance of a football player. Get the name and the number of maches.
+    For each match, get the numbers of goals the player scores.
+    In the end, put all the data in a dict, including the total of goals scored by the player.
+'''
+
+division = '*='
+count = 30
+divisor = division * count
+
+player = {
+    'name': input('Player Name: ').strip().title(),
+    'matches': int(input('Matches played: ')),
+    'goals': list(),
+    'total': 0
+}
+
+for match in range(0, player['matches']):
+    goals = int(input(f'Goals in the \033[32m{match + 1}º\033[m match? '))
+    player['goals'].append(goals)
+
+    player['total'] += goals
+
+player.__delitem__('matches')
+
+print(divisor)
+
+print(player)
+
+print(divisor)
+
+for key, value in player.items():
+    print(f'The key \033[32m{key}\033[m has value of \033[34m{value}\033[m')
+
+print(divisor)
+
+print(f"The player called \033[32m{player['name']}\033[m plays \033[34m{len(player['goals'])}\033[m matches.")
+
+for index, value in enumerate(player['goals']):
+    print(f'\t=> In the \033[32m{index + 1}º\033[m match, \033[34m{value}\033[m goals.')
