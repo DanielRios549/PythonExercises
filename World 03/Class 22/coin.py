f790c3f6a03dee433764a9f5a33aedf2ecf4e7e1
Daniel Rios <danielrios.flamengo@gmail.com>
2021-12-22 19:50:15 -0300

    Add Summary

--- World 03/Class 22/coin.py
+++ World 03/Class 22/coin.py
@@ -2,6 +2,13 @@ def format(number: float):
     return f'R${number:.2f}'.replace('.', ',')
 
 
+def summary(number: float, increase: int = 10, decrease: int = 10):
+    print(f'The half of \033[34m{format(number)}\033[m is \033[32m{half(number, True)}\033[m')
+    print(f'The double of \033[34m{format(number)}\033[m is \033[32m{double(number, True)}\033[m')
+    print(f'Increasing \033[34m{increase}%\033[m, the new price is \033[32m{increment(number, increase, True)}\033[m')
+    print(f'Decreasing \033[34m{decrease}%\033[m, the new price is \033[32m{decrement(number, decrease, True)}\033[m')
+
+
 def increment(number: float, percent: int, coin: bool = False):
     result = number + (number * percent / 100)
 
